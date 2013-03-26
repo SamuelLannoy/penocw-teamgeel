ec16d4249168b1a9280424faab5c793e2258783f
Ruben <Ruben@10.43.9.27>
2013-03-26 09:17:00 +0100

    converter

---
+++ penopc/src/field/fieldmerge/TileConverter.java
@@ -0,0 +1,32 @@
+package field.fieldmerge;
+
+import java.util.Collection;
+
+import field.Border;
+import field.Field;
+import field.Position;
+import field.Tile;
+import field.fromfile.MazePart;
+
+public class TileConverter {
+	
+	
+	public static void convertToFieldTile(peno.htttp.Tile tileMsg, Field field) {
+		String[] split = tileMsg.getToken().split("\\.");
+		MazePart part = MazePart.getPartFromString(split[0]);
+		
+		Position tilePos = new Position((int)tileMsg.getX(), (int)tileMsg.getY());
+		Tile added = null;
+		if (field.canHaveAsTile(tilePos)){
+			added = new Tile(tilePos);
+			field.addTile(added);
+		} else {
+			added = field.getTileMap().getObjectAtId(tilePos);
+		}
+		
+		Collection<Border> borders = part.getBorders(split.length >= 2 ? split[1] : "", added);
+		
+		field.addBorders(borders);
+	}
+
+}
