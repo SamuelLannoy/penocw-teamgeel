c90f161dc9ad3c390137d9b2f116890428827706
robingoossens <robin.goossens1@student.kuleuven.be>
2013-03-26 11:57:23 +0100

    ding in while lus aangepast

--- penopc/src/robot/brain/Explorer.java
+++ penopc/src/robot/brain/Explorer.java
@@ -98,7 +98,8 @@ public class Explorer {
 
 			boolean ignoreSeesaw = false;
 
-			//while (!robot.getCurrTile().getPosition().equals(current.getTile().getPosition())) {
+			while (!robot.getCurrTile().getPosition().equals(current.getTile().getPosition())) {
+				System.out.println(robot.getCurrTile().getPosition() + " test "+ current.getTile().getPosition());
 				List<Tile> tileList = Pathfinder.findShortestPath(robot, current.getTile(), ignoreSeesaw);
 				//System.out.println("list: " + tileList.toString());
 				robot.setAStartTileList(tileList);
@@ -112,13 +113,13 @@ public class Explorer {
 						// het pad wordt tot dan afgelegd en vervolgens opnieuw berekend.
 
 						//TODO:add
-						/*Tile tile = robot.getField().getTileMap().getObjectAtId(tileList.get(i).getPosition());
+						Tile tile = robot.getField().getTileMap().getObjectAtId(tileList.get(i).getPosition());
 						if (tile.getBarcode() != null && (tile.getBarcode().getDecimal() == 11 || tile.getBarcode().getDecimal() == 13 ||
 								tile.getBarcode().getDecimal() == 15 || tile.getBarcode().getDecimal() == 17 || tile.getBarcode().getDecimal() == 19 ||
 								tile.getBarcode().getDecimal() == 21)){
 							brokeLoop = true;
 							break;
-						}*/
+						}
 						robot.travelFromTileToTile(tileList.get(i), tileList.get(i+1), tileList.get(i-1));
 					}
 					//DebugBuffer.addInfo("turning on barcode read");
@@ -129,12 +130,11 @@ public class Explorer {
 					//TODO:add
 					// de wip was open en de robot is erover gegaan. 
 					// Zet de robot op de positie na de wip.
-					/*if (brokeLoop) {
+					if (brokeLoop) {
 						waitTillRobotStops(robot, 1000);
-					}
+					
 					Direction dirForw = Direction.fromAngle(robot.getPosition().getRotation());
-
-					/*if (SensorBuffer.getInfrared() < 4) {
+					if (SensorBuffer.getInfrared() < 4) {
 						Tile tile = robot.getCurrTile();
 
 						robot.moveAcrossSeesaw();
@@ -150,8 +150,8 @@ public class Explorer {
 						ignoreSeesaw = false;
 					} else {
 						ignoreSeesaw = true;
-					}*/
-
+					}
+					}
 
 					if (tileList.size() > 2) {
 						robot.hasWrongBarcode();
@@ -163,7 +163,7 @@ public class Explorer {
 
 					DebugBuffer.addInfo("done moving!");
 				}
-			//}
+			}
 			Direction dirForw = Direction.fromAngle(robot.getPosition().getRotation());
 			System.out.println("dirforw " + dirForw);
 			Direction dirLeft = Direction.fromAngle(robot.getPosition().getRotation() - 90); 
